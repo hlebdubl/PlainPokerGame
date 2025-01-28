b7d9cd9d1a2c71805efff8cbe447ba3dd31ee21d
BT_2S14_24 <michaelk368@nycstudents.net>
2025-01-28 10:14:13 -0500

    Initial file handling, added sample hands to data file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        // step 1: create a file object
+        File f = new File("data/cards");
 
-        System.out.println("CARD GAME HERE");
+        String fileData = "";
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                fileData += currentLine + "\n";
+            }
+
+            // a String array where every item in the array is a line from the file
+            String[] fileArray = fileData.split("\n");
+
+            for (String line : fileArray) {
+                // split by comma, now we have a list of String numbers
+                String[] stringHands = line.split(",");
+                stringHands = line.split("\\|");
+                System.out.println(Arrays.toString(stringHands));
+            }
+        }
+        catch (FileNotFoundException fe) {
+            System.out.println("File was not found");
+            System.exit(1);
+        }
+
     }
 }
