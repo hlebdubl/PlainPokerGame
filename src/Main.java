84404274d39688f579ea5c4db1fbc69e3661e428
Hleb <hlebp@nycstudents.net>
2025-02-04 10:45:57 -0500

    Final optimization of code

--- src/Main.java
+++ src/Main.java
@@ -35,15 +35,19 @@ public class Main {
             String[] fileArray = fileData.split("\n");
 
             Hand[] handList = new Hand[fileArray.length];
+            Hand[] jackList = new Hand[fileArray.length];
+
 
             for (int i = 0; i < fileArray.length; i++) {
 
                 String[] stringHands = fileArray[i].split(",|\\|");
 
+                Hand j = new Hand(stringHands);
                 Hand h = new Hand(stringHands);
 
                 //detect hands and add them to total counts
                 int handType = h.detectHands(false);
+                int jackType = j.detectHands(true);
                 switch (handType)
                 {
                     case 7:
@@ -69,6 +73,9 @@ public class Main {
                         break;
                 }
 
+
+                //detect hands and add them to total counts
+                jackList[i] = j;
                 handList[i] = h;
             }
 
@@ -77,6 +84,12 @@ public class Main {
             for (int i = 0; i < handList.length; i++) //getting rank from sorted list and using it for totalBidVal
             {
                 totalBidVal += (handList[i].getBid() * (handList.length - i));
+            }
+            jackList = Hand.sortHandList(jackList, true);
+
+            for (int i = 0; i < jackList.length; i++) //getting rank from sorted list and using it for totalBidVal
+            {
+                totalJackValue += (jackList[i].getBid() * (jackList.length - i));
             }
 
             System.out.println("Five of a kind: " + fiveKind);
@@ -87,35 +100,13 @@ public class Main {
             System.out.println("Pairs: " + pair);
             System.out.println("High cards: " + high);
             System.out.println("Total bid value: " + (totalBidVal));
+            System.out.println("Total bid value with Jacks Wild: " + totalJackValue);
 
-
-            Hand[] jackList = new Hand[fileArray.length];
-
-            for (int i = 0; i < fileArray.length; i++) {
-
-                String[] stringHands = fileArray[i].split(",|\\|");
-
-                Hand j = new Hand(stringHands);
-
-                //detect hands and add them to total counts
-                int handType = j.detectHands(true);
-                jackList[i] = j;
-            }
-
-            jackList = Hand.sortHandList(jackList, true);
-
-            for (int i = 0; i < jackList.length; i++) //getting rank from sorted list and using it for totalBidVal
-            {
-                totalJackValue += (jackList[i].getBid() * (jackList.length - i));
-            }
 
         }
         catch (FileNotFoundException fe) {
             System.out.println("File was not found");
             System.exit(1);
         }
-
-        System.out.println("Total bid value with Jacks Wild: " + totalJackValue);
-
     }
 }
